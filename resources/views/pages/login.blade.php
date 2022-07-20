e00ce5d1aba9e8ac7c8a353429bd24454145cf23
Yulianas24 <101847329+Yulianas24@users.noreply.github.com>
2022-07-21 00:20:41 +0700

    tambahin modal di login jika registrasi berhasil

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite('resources/css/app.css')
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <title>{{ $title }}</title>
 </head>
 <body>
@@ -60,63 +61,34 @@
 </div>
 
 
-{{-- Modal  --}}
-@if (session()->has('success'))
-<div class="modal fade fixed top-0 left-0 hidden w-full h-full outline-none overflow-x-hidden overflow-y-auto"
-id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="false">
-    <div class="modal-dialog relative w-auto pointer-events-none">
-        <div
-        class="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding rounded-md outline-none text-current">
-        <div
-        class="modal-header flex flex-shrink-0 items-center justify-between p-4 border-b border-gray-200 rounded-t-md">
-        <h5 class="text-xl font-medium leading-normal text-gray-800" id="exampleModalLabel">Modal title</h5>
-        <button type="button"
-            class="btn-close box-content w-4 h-4 p-1 text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline"
-            data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body relative p-4">
-        Modal body text goes here.
-        </div>
-        <div
-        class="modal-footer flex flex-shrink-0 flex-wrap items-center justify-end p-4 border-t border-gray-200 rounded-b-md">
-        <button type="button" class="px-6
-        py-2.5
-            bg-purple-600
-            text-white
-            font-medium
-            text-xs
-            leading-tight
-            uppercase
-            rounded
-            shadow-md
-            hover:bg-purple-700 hover:shadow-lg
-            focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0
-            active:bg-purple-800 active:shadow-lg
-            transition
-            duration-150
-            ease-in-out" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="px-6
-        py-2.5
-        bg-blue-600
-        text-white
-        font-medium
-        text-xs
-        leading-tight
-        uppercase
-        rounded
-        shadow-md
-        hover:bg-blue-700 hover:shadow-lg
-        focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
-        active:bg-blue-800 active:shadow-lg
-        transition
-        duration-150
-        ease-in-out
-        ml-1">Save changes</button>
-            </div>
-        </div>
+{{-- Modal jika registrasi berhasil --}}
+
+{{-- editin Zak 
+    Bikin popup registrasi berhasil
+    aku mau tidur
+    Hapus comment di if session kalo udah pas
+    --}}
+
+{{-- @if (session()->has('success'))  --}} 
+
+<div id="id01" class="w3-modal" style="display: block">
+    
+    <div class="w3-modal-content w3-animate-top w3-card-4">
+      <header class="w3-container w3-teal"> 
+        <span onclick="document.getElementById('id01').style.display='none'" 
+        class="w3-button w3-display-topright">&times;</span>
+        <h2>Modal Header</h2>
+      </header>
+      <div class="w3-container">
+        <p>Some text..</p>
+        <p>Some text..</p>
+      </div>
+      <footer class="w3-container w3-teal">
+        <p>Modal Footer</p>
+      </footer>
     </div>
 </div>
-        @endif
+{{-- @endif --}}
 
 </body>
 </html>
