aed3911dbb0c8cf27c9f0e05f27a79a6f8ba2c4d
CoreSinText <muhammadrazak4@gmail.com>
2022-07-23 21:25:39 +0700

    update halaman fitur

--- resources/views/components/fitur.blade.php
+++ resources/views/components/fitur.blade.php
@@ -5,13 +5,10 @@
             <span class="w-2/6 h-0.5 bg-blue-902"></span>
         </figcaption>
 
-        <figure class="flex justify-between mt-16 items-center w-full h-full">
+        <figure class="flex justify-between mt-16 items-center w-full h-5/6">
             {{-- ?: Picture --}}
-            <figcaption class="relative flex h-full">
-                <picture><img src="/asset/img/cardImageFitur.png" alt=""></picture>
-                <picture class="h-80 w-80 bg-blue-902 absolute -right-1 -top-1 -z-10"></picture>
-                <picture class="h-80 w-80 bg-blue-902 absolute -left-1 -bottom-1 -z-10"></picture>
-            </figcaption>
+
+            <picture><img src="/asset/img/cardImageFitur.svg" alt=""></picture>
             {{-- ?: Fitur --}}
             <figcaption class="flex flex-col h-full w-95 justify-between ">
                 <p class="font-poppins text-lg">Fitur yag akan membantu anda untuk menyelesaikan masalah anda</p>
@@ -20,7 +17,7 @@
                 <figure class="flex flex-col justify-evenly h-full w-full">
                     {{-- ?: Container Detail Fitur --}}
                     {{-- !: Deteksi Mandiri --}}
-                    <figcaption class="flex flex-col mt-6">
+                    <figcaption class="flex flex-col">
                         <picture class="flex items-center">
                             <img src="/asset/icons/microscope.svg" alt="microscope" srcset="" class="h-12 w-12">
                             <h3 class="font-roboto font-medium text-2xl ml-4">Deteksi Mandiri</h3>
@@ -32,7 +29,7 @@
                             dengan cepat</p>
                     </figcaption>
                     {{-- !: Cari Dan Booking Konselor --}}
-                    <figcaption class="flex flex-col mt-6">
+                    <figcaption class="flex flex-col">
                         <picture class="flex items-center">
                             <img src="/asset/icons/search.svg" alt="microscope" srcset="" class="h-12 w-12">
                             <h3 class="font-roboto font-medium text-2xl ml-4">Cari dan Booking Konselor</h3>
@@ -41,7 +38,7 @@
                             CareYou dengan mudah dan praktis</p>
                     </figcaption>
                     {{-- !: Konsultasi --}}
-                    <figcaption class="flex flex-col mt-6">
+                    <figcaption class="flex flex-col">
                         <picture class="flex items-center">
                             <img src="/asset/icons/human.svg" alt="microscope" srcset="" class="h-12 w-12">
                             <h3 class="font-roboto font-medium text-2xl ml-4">Konsultasi</h3>
