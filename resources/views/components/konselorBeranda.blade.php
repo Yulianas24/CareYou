50e920ea240a6de7296f3108660ed289bef7d8ab
CoreSinText <muhammadrazak4@gmail.com>
2022-07-24 05:14:28 +0700

    halaman konselor

--- resources/views/components/konselorBeranda.blade.php
+++ resources/views/components/konselorBeranda.blade.php
@@ -1,3 +1,21 @@
-<div class="w-screen h-screen" id="pageKonselor">
+<div class="absolute -z-50 w-screen h-screen">
     @include('template/templateKonselor')
+</div>
+<div class="w-screen h-screen flex flex-col justify-evenly items-center " id="pageKonselor">
+    {{-- ?: Container Text --}}
+    <figure class="flex justify-between w-10/12 mt-20">
+        <figcaption class="flex flex-col">
+            <h1 class="font-roboto font-medium text-3xl">Konselor CareYou</h1>
+            <span class="h-0.5 w-75 bg-blue-902"></span>
+        </figcaption>
+        <figcaption class="flex items-center cursor-pointer">
+            <a href="#pageHome" class="font-roboto font-semibold text-base text-blue-902">Lihat Semua</a>
+            <img src="/asset/icons/arrow_back.svg" alt="" class="bg-cover">
+        </figcaption>
+    </figure>
+
+    {{-- ?: Card Container --}}
+    <figure class="flex w-full h-4/5 justify-evenly items-center">
+        @include('/partials/cardKonselor')
+    </figure>
 </div>
---
+++ resources/views/partials/cardKonselor.blade.php
@@ -0,0 +1,92 @@
+{{-- !: Card 1 --}}
+<div class="h-102 w-94 rounded-3xl flex flex-col bg-white justify-start">
+    <picture><img src="/asset/img/cardKonselor.png" alt="" srcset=""></picture>
+    <figure class="text-center font-roboto font-medium text-lg">
+        Rayna Vaccaro, S.Psi., M.Psi
+    </figure>
+    {{-- ?: Container Penanganan --}}
+    <figure class="flex flex-col items-center h-44 w-full mt-3">
+        <figcaption class="flex w-1/1.1 ">
+            <h3 class="font-roboto font-medium text-lg">Fokus Penaganan masalah :</h3>
+        </figcaption>
+        <figcaption class="h-full w-1/1.2">
+            <ul class="flex flex-col h-full w-full justify-evenly">
+                <li class="flex font-roboto"><img src="/asset/icons/checklist.svg" alt="" class="mx-2"> Stress
+                </li>
+                <li class="flex font-roboto"><img src="/asset/icons/checklist.svg" alt=""
+                        class="mx-2">Deppression</li>
+                <li class="flex font-roboto"><img src="/asset/icons/checklist.svg" alt="" class="mx-2">Toxic
+                    Relationship</li>
+                <li class="flex font-roboto"><img src="/asset/icons/checklist.svg" alt="" class="mx-2">Stress
+                </li>
+            </ul>
+        </figcaption>
+    </figure>
+    {{-- ?: Container Button --}}
+    <figure class="flex w-full h-full items-center justify-center">
+        <figcaption class="flex items-center bg-blue-902 h-10 px-7 text-white rounded-lg">Lihat Profil Lengkap
+        </figcaption>
+    </figure>
+</div>
+
+{{-- !: Card 1 --}}
+<div class="h-102 w-94 rounded-3xl flex flex-col bg-white justify-start">
+    <picture><img src="/asset/img/cardKonselor.png" alt="" srcset=""></picture>
+    <figure class="text-center font-roboto font-medium text-lg">
+        Rayna Vaccaro, S.Psi., M.Psi
+    </figure>
+    {{-- ?: Container Penanganan --}}
+    <figure class="flex flex-col items-center h-44 w-full mt-3">
+        <figcaption class="flex w-1/1.1 ">
+            <h3 class="font-roboto font-medium text-lg">Fokus Penaganan masalah :</h3>
+        </figcaption>
+        <figcaption class="h-full w-1/1.2">
+            <ul class="flex flex-col h-full w-full justify-evenly">
+                <li class="flex font-roboto"><img src="/asset/icons/checklist.svg" alt="" class="mx-2"> Stress
+                </li>
+                <li class="flex font-roboto"><img src="/asset/icons/checklist.svg" alt=""
+                        class="mx-2">Deppression</li>
+                <li class="flex font-roboto"><img src="/asset/icons/checklist.svg" alt="" class="mx-2">Toxic
+                    Relationship</li>
+                <li class="flex font-roboto"><img src="/asset/icons/checklist.svg" alt="" class="mx-2">Stress
+                </li>
+            </ul>
+        </figcaption>
+    </figure>
+    {{-- ?: Container Button --}}
+    <figure class="flex w-full h-full items-center justify-center">
+        <figcaption class="flex items-center bg-blue-902 h-10 px-7 text-white rounded-lg">Lihat Profil Lengkap
+        </figcaption>
+    </figure>
+</div>
+
+{{-- !: Card 1 --}}
+<div class="h-102 w-94 rounded-3xl flex flex-col bg-white justify-start">
+    <picture><img src="/asset/img/cardKonselor.png" alt="" srcset=""></picture>
+    <figure class="text-center font-roboto font-medium text-lg">
+        Rayna Vaccaro, S.Psi., M.Psi
+    </figure>
+    {{-- ?: Container Penanganan --}}
+    <figure class="flex flex-col items-center h-44 w-full mt-3">
+        <figcaption class="flex w-1/1.1 ">
+            <h3 class="font-roboto font-medium text-lg">Fokus Penaganan masalah :</h3>
+        </figcaption>
+        <figcaption class="h-full w-1/1.2">
+            <ul class="flex flex-col h-full w-full justify-evenly">
+                <li class="flex font-roboto"><img src="/asset/icons/checklist.svg" alt="" class="mx-2"> Stress
+                </li>
+                <li class="flex font-roboto"><img src="/asset/icons/checklist.svg" alt=""
+                        class="mx-2">Deppression</li>
+                <li class="flex font-roboto"><img src="/asset/icons/checklist.svg" alt="" class="mx-2">Toxic
+                    Relationship</li>
+                <li class="flex font-roboto"><img src="/asset/icons/checklist.svg" alt="" class="mx-2">Stress
+                </li>
+            </ul>
+        </figcaption>
+    </figure>
+    {{-- ?: Container Button --}}
+    <figure class="flex w-full h-full items-center justify-center">
+        <figcaption class="flex items-center bg-blue-902 h-10 px-7 text-white rounded-lg">Lihat Profil Lengkap
+        </figcaption>
+    </figure>
+</div>
