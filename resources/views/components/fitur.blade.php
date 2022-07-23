7ba0c23311ca05f95deaf2a48a5cb0febfeb9a16
CoreSinText <muhammadrazak4@gmail.com>
2022-07-23 18:15:19 +0700

    halaman fitur

--- resources/views/components/fitur.blade.php
+++ resources/views/components/fitur.blade.php
@@ -1,12 +1,56 @@
 <div class="flex w-screen h-screen justify-center items-center" id="pageFitur">
-    <figure class="flex flex-col w-10/12 h-5/6">
+    <figure class="flex flex-col w-10/12 h-4/5">
         <figcaption class="flex flex-col w-full">
             <h1 class="font-roboto font-medium justify-between text-3xl h-14">Fitur Care You</h1>
             <span class="w-2/6 h-0.5 bg-blue-902"></span>
         </figcaption>
 
-        <figure class="flex items-end w-full h-full">
-            <picture><img src="/asset/img/cardImageFitur.png" alt=""></picture>
+        <figure class="flex justify-between mt-16 items-center w-full h-full">
+            {{-- ?: Picture --}}
+            <figcaption class="relative flex h-full">
+                <picture><img src="/asset/img/cardImageFitur.png" alt=""></picture>
+                <picture class="h-80 w-80 bg-blue-902 absolute -right-1 -top-1 -z-10"></picture>
+                <picture class="h-80 w-80 bg-blue-902 absolute -left-1 -bottom-1 -z-10"></picture>
+            </figcaption>
+            {{-- ?: Fitur --}}
+            <figcaption class="flex flex-col h-full w-95 justify-between ">
+                <p class="font-poppins text-lg">Fitur yag akan membantu anda untuk menyelesaikan masalah anda</p>
+
+                {{-- ?: Container Detail Fitur --}}
+                <figure class="flex flex-col justify-evenly h-full w-full">
+                    {{-- ?: Container Detail Fitur --}}
+                    {{-- !: Deteksi Mandiri --}}
+                    <figcaption class="flex flex-col mt-6">
+                        <picture class="flex items-center">
+                            <img src="/asset/icons/microscope.svg" alt="microscope" srcset="" class="h-12 w-12">
+                            <h3 class="font-roboto font-medium text-2xl ml-4">Deteksi Mandiri</h3>
+                        </picture>
+                        <p class="font-poppins text-base text-gray-500 mt-3">Deteksi secara mandiri kesehatan mentalmu
+                            dengan
+                            mengisi
+                            assesmen dan dapatkan hasilnya
+                            dengan cepat</p>
+                    </figcaption>
+                    {{-- !: Cari Dan Booking Konselor --}}
+                    <figcaption class="flex flex-col mt-6">
+                        <picture class="flex items-center">
+                            <img src="/asset/icons/search.svg" alt="microscope" srcset="" class="h-12 w-12">
+                            <h3 class="font-roboto font-medium text-2xl ml-4">Cari dan Booking Konselor</h3>
+                        </picture>
+                        <p class="font-poppins text-base text-gray-500 mt-3">Cari dan booking konselor pilihanmu di
+                            CareYou dengan mudah dan praktis</p>
+                    </figcaption>
+                    {{-- !: Konsultasi --}}
+                    <figcaption class="flex flex-col mt-6">
+                        <picture class="flex items-center">
+                            <img src="/asset/icons/human.svg" alt="microscope" srcset="" class="h-12 w-12">
+                            <h3 class="font-roboto font-medium text-2xl ml-4">Konsultasi</h3>
+                        </picture>
+                        <p class="font-poppins text-base text-gray-500 mt-3">Konsultasikan masalahmu pada konselor
+                            CareYou untuk mendapatkan solusi terbaik</p>
+                    </figcaption>
+                </figure>
+            </figcaption>
         </figure>
     </figure>
 </div>
