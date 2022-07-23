72591dfe58d3f5548a58583145df25d89ec3cfc6
CoreSinText <muhammadrazak4@gmail.com>
2022-07-23 18:20:49 +0700

    update halaman fitur

--- resources/views/components/fitur.blade.php
+++ resources/views/components/fitur.blade.php
@@ -1,5 +1,5 @@
 <div class="flex w-screen h-screen justify-center items-center" id="pageFitur">
-    <figure class="flex flex-col w-10/12 h-4/5">
+    <figure class="grid-cols-2 w-10/12 h-4/5">
         <figcaption class="flex flex-col w-full">
             <h1 class="font-roboto font-medium justify-between text-3xl h-14">Fitur Care You</h1>
             <span class="w-2/6 h-0.5 bg-blue-902"></span>
