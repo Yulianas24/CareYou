3448ffa7124b0e88c3e1c0808edf4536408c8680
CoreSinText <muhammadrazak4@gmail.com>
2022-07-28 11:58:19 +0700

    halaman profile

---
+++ public/js/profile.js
@@ -0,0 +1,9 @@
+// ?: Container Photo
+const inputPhoto = document.querySelector("[input-photo]");
+const buttonInputPhoto = document.querySelector("[button-input-photo]");
+// todo: input Photo Trigger
+buttonInputPhoto.addEventListener("click", () => {
+    inputPhoto.click();
+});
+
+// ?: Container InputText
