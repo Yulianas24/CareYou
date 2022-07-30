2e1444e1cbba8557657b2593acbac10f4f36bd2d
CoreSinText <muhammadrazak4@gmail.com>
2022-07-30 08:33:21 +0700

    update artikel beranda

--- resources/views/components/artikelBeranda.blade.php
+++ resources/views/components/artikelBeranda.blade.php
@@ -6,19 +6,19 @@
                 <span class="h-0.5 w-75 bg-blue-902"></span>
                 <br>
             </figcaption>
-      
+
             <figcaption class=" flex justify-center ">
-                <div class="relative w-80 h-52 lg:mx-24 mb-14 rounded-lg bg-black">
+                <div class="relative w-80 h-52 mx-24 mb-14 rounded-lg bg-black">
                     <a href="/posts?category=stress">
                         <div class="absolute z-40 w-full left-2 top-24">
                             <h1 class="font-roboto font-medium text-2xl text-white">Stress</h1>
                             <div class="h-0.5 w-1/2 bg-white"></div>
                         </div>
                         <img src="\asset\img\stress.jpg" alt="" class="rounded-lg w-full h-full opacity-60">
-                        
+
                     </a>
                 </div>
-                <div class="relative w-80 h-52 lg:mx-24 mb-14 rounded-lg bg-black">
+                <div class="relative w-80 h-52 mx-24 mb-14 rounded-lg bg-black">
                     <a href="/posts?category=depresi">
                         <div class="absolute z-40 w-full left-2 top-24">
                             <h1 class="font-roboto font-medium text-2xl text-white">Depresi</h1>
@@ -30,7 +30,7 @@
 
             </figcaption>
             <figcaption class=" flex justify-center ">
-                <div class="relative w-80 h-52 lg:mx-24 mb-14 rounded-lg bg-black">
+                <div class="relative w-80 h-52 mx-24 mb-14 rounded-lg bg-black">
                     <a href="/posts?category=kecemasan">
                         <div class="absolute z-40 w-full left-2 top-24">
                             <h1 class="font-roboto font-medium text-2xl text-white">Kecemasan</h1>
@@ -40,19 +40,20 @@
 
                     </a>
                 </div>
-                <div class="relative w-80 h-52 lg:mx-24 mb-14 rounded-lg bg-black">
+                <div class="relative w-80 h-52 mx-24 mb-14 rounded-lg bg-black">
                     <a href="/posts?category=toxic relationship">
                         <div class="absolute z-40 w-full left-2 top-24">
                             <h1 class="font-roboto font-medium text-2xl text-white">Toxic Relationship</h1>
                             <div class="h-0.5 w-1/2 bg-white"></div>
                         </div>
-                        <img src="\asset\img\toxic_relationship.jpg" alt="" class="rounded-lg w-full h-full opacity-60">
+                        <img src="\asset\img\toxic_relationship.jpg" alt=""
+                            class="rounded-lg w-full h-full opacity-60">
 
                     </a>
                 </div>
-              
+
             </figcaption>
         </figure>
-      </figure>
-      
+    </figure>
+
 </div>
