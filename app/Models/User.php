9932d0440ce04d802a944018cf0516bda6e0c547
Yulianas24 <101847329+Yulianas24@users.noreply.github.com>
2022-07-31 17:01:43 +0700

    tambah relasi jadwal ke user

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,4 +47,9 @@ class User extends Authenticatable
     {
         return $this->hasOne(counselorProfile::class);
     }
+
+    public function jadwal()
+    {
+        return $this->hasMany(jadwalCounselor::class);
+    }
 }
