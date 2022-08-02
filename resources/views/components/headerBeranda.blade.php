f37c8cf3a6f24ef72783181b97b26ac23e425179
CoreSinText <muhammadrazak4@gmail.com>
2022-08-02 16:00:00 +0700

    Responsive Home

--- resources/views/components/headerBeranda.blade.php
+++ resources/views/components/headerBeranda.blade.php
@@ -3,18 +3,21 @@
     <div class=" h-full w-full bg-black opacity-80 absolute -z-10"></div>
     {{-- ?: Image Background --}}
     <img src="/asset/img/backgroundHome.png" alt="backgroundBeranda" srcset=""
-        class="-z-20 w-full h-full absolute backdrop-grayscale">
+        class="object-cover -z-50 h-screen w-screen absolute bg-scroll">
     {{-- ?: Container Intro --}}
     <figure class="flex flex-col w-full h-full justify-center items-center z-20">
         {{-- ?: Sub Container --}}
-        <div class="w-105 h-80 relative">
+        <div class="relative w-[90vw] laptop:w-105 laptop:h-80">
             {{-- ?: Text Header --}}
-            <figcaption class="text-white font-roboto font-semibold text-6xl text-center">Cek Kesehatan Mentalmu dan
+            <figcaption class="text-white font-roboto font-semibold text-center text-3xl tablet:5xl laptop:text-6xl">Cek
+                Kesehatan
+                Mentalmu
+                dan
                 <span class="text-yellow-901">"</span>Konsultasi<span class="text-yellow-901">"</span> pada
                 Konselor
                 Terpercaya
                 <img src="/asset/theme/Vector1.svg" alt="Image Text Header" srcset=""
-                    class="absolute right-0 top-16">
+                    class="hidden absolute right-0 top-16 laptop:block">
             </figcaption>
             <figcaption class="text-white text-center font-poppins pt-8">Cek kesehatan mentalmu dengan mudah dan praktis
             </figcaption>
