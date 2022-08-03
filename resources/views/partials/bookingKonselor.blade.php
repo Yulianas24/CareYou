1e19bf951027151a1e7b222e94ba502a31164b84
CoreSinText <muhammadrazak4@gmail.com>
2022-08-04 04:21:59 +0700

    Responsive detail konselor

---
+++ resources/views/partials/bookingKonselor.blade.php
@@ -0,0 +1,28 @@
+ {{-- ?: Rounded Card --}}
+ <div opsi-konsultasi class="rounded-2xl top-24 h-94 w-1/1.2 z-50 bg-white outline outline-gray-300 shadow-lg">
+     <div class="flex flex-col w-full h-full mt-4 p-2">
+         {{-- !: Header --}}
+         <div class="flex w-full justify-between">
+             <h1 class="font-roboto text-xl border-b-2 border-blue-902 pr-4">Pilih Konsultasi</h1>
+             <img src="/asset/icons/closeIcon.svg" alt="close icon" class="h-8 pr-2 cursor-pointer" close-pop-up-booking>
+         </div>
+
+         {{-- !: Metode Konsultasi --}}
+         <div class="flex justify-between w-1/1.2 mt-8">
+             {{-- todo: Online --}}
+             <div class="flex flex-col w-[48%]">
+                 <h3 class="font-roboto my-2 text-lg font-bold border-b-2 border-blue-902">Online</h3>
+                 <p class="font-roboto my-2">Via Chat, Voice, Video Call</p>
+                 <button class="font-roboto my-2 bg-blue-902 py-3 px-1 text-white rounded-lg">Pilih Konsultasi
+                     Online</button>
+             </div>
+             <div class="flex flex-col w-[48%]">
+                 <h3 class="font-roboto my-2 text-lg font-bold border-b-2 border-blue-902">Offline</h3>
+                 <p class="font-roboto my-2">Bertemu langsung dengan konselor</p>
+                 <button booking-konselor class="font-roboto my-2 bg-blue-902 py-3 px-1 text-white rounded-lg">Pilih
+                     Konsultasi
+                     Online</button>
+             </div>
+         </div>
+     </div>
+ </div>
