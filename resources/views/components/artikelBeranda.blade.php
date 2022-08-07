af856d677dfb84b457f56e83400d2a99a596dccd
CoreSinText <muhammadrazak4@gmail.com>
2022-08-08 06:02:13 +0700

    Responsive artikel beranda

--- resources/views/components/artikelBeranda.blade.php
+++ resources/views/components/artikelBeranda.blade.php
@@ -1,59 +1,54 @@
-<div class="h-screen w-screen pt-4" id="pageArtikel">
-    <figure class="h-full w-full bg-white flex justify-center items-center">
-        <figure class="h-5/6 w-5/6 flex flex-col justify-start">
-            <figcaption class="flex flex-col">
-                <h1 class="font-roboto font-medium text-2xl">Artikel</h1>
-                <span class="h-0.5 w-75 bg-blue-902"></span>
-                <br>
-            </figcaption>
-
-            <figcaption class=" flex justify-center ">
-                <div class="relative w-80 h-52 mx-24 mb-14 rounded-lg bg-black">
-                    <a href="/posts?category=stress">
-                        <div class="absolute z-40 w-full left-2 top-24">
-                            <h1 class="font-roboto font-medium text-2xl text-white">Stress</h1>
-                            <div class="h-0.5 w-1/2 bg-white"></div>
-                        </div>
-                        <img src="\asset\img\stress.jpg" alt="" class="rounded-lg w-full h-full opacity-60">
-
-                    </a>
-                </div>
-                <div class="relative w-80 h-52 mx-24 mb-14 rounded-lg bg-black">
-                    <a href="/posts?category=depresi">
-                        <div class="absolute z-40 w-full left-2 top-24">
-                            <h1 class="font-roboto font-medium text-2xl text-white">Depresi</h1>
-                            <div class="h-0.5 w-1/2 bg-white"></div>
-                        </div>
-                        <img src="\asset\img\depresi.jpg" alt="" class="rounded-lg w-full h-full opacity-60">
-                    </a>
-                </div>
-
-            </figcaption>
-            <figcaption class=" flex justify-center ">
-                <div class="relative w-80 h-52 mx-24 mb-14 rounded-lg bg-black">
-                    <a href="/posts?category=kecemasan">
-                        <div class="absolute z-40 w-full left-2 top-24">
-                            <h1 class="font-roboto font-medium text-2xl text-white">Kecemasan</h1>
-                            <div class="h-0.5 w-1/2 bg-white"></div>
-                        </div>
-                        <img src="\asset\img\kecemasan.jpg" alt="" class="rounded-lg w-full h-full opacity-60">
-
-                    </a>
-                </div>
-                <div class="relative w-80 h-52 mx-24 mb-14 rounded-lg bg-black">
-                    <a href="/posts?category=toxic relationship">
-                        <div class="absolute z-40 w-full left-2 top-24">
-                            <h1 class="font-roboto font-medium text-2xl text-white">Toxic Relationship</h1>
-                            <div class="h-0.5 w-1/2 bg-white"></div>
-                        </div>
-                        <img src="\asset\img\toxic_relationship.jpg" alt=""
-                            class="rounded-lg w-full h-full opacity-60">
-
-                    </a>
-                </div>
-
-            </figcaption>
-        </figure>
-    </figure>
-
+<div class="min-h-screen w-screen px-2 flex-col desktop:px-16">
+    <h1 class="font-roboto text-2xl border-b-2 border-blue-601 w-fit pb-3 pr-4">Artikel</h1>
+    <div class="w-full flex justify-center">
+        <div class="flex justify-evenly flex-wrap w-full min-h-fit desktop:w-5/6">
+            {{-- !: Card Stress --}}
+            <div class="relative m-10">
+                <a href="/posts?category=stress">
+                    <div wrap-image class="bg-gray-800/70 z-20 absolute rounded-lg"></div>
+                    <img image-gendre src="\asset\img\stress.jpg" alt="" class="rounded-lg z-10 bg-cover w-96">
+                    <h2
+                        class="font-roboto text-3xl font-bold absolute left-8 top-1/2 z-30 text-white border-b-2 border-white pr-20">
+                        Stress
+                    </h2>
+                </a>
+            </div>
+            {{-- !: Card Depresi --}}
+            <div class="relative m-10">
+                <a href="/posts?category=depresi">
+                    <div wrap-image class="bg-gray-800/70 z-20 absolute rounded-lg"></div>
+                    <img image-gendre src="\asset\img\depresi.jpg" alt="" class="rounded-lg z-10 bg-cover w-96">
+                    <h2
+                        class="font-roboto text-3xl font-bold absolute left-8 top-1/2 z-30 text-white border-b-2 border-white pr-20">
+                        Depresi
+                    </h2>
+                </a>
+            </div>
+            {{-- !: Card Kecemasan --}}
+            <div class="relative m-10">
+                <a href="/posts?category=kecemasan">
+                    <div wrap-image class="bg-gray-800/70 z-20 absolute rounded-lg"></div>
+                    <img image-gendre src="\asset\img\kecemasan.jpg" alt=""
+                        class="rounded-lg z-10 bg-cover w-96">
+                    <h2
+                        class="font-roboto text-3xl font-bold absolute left-8 top-1/2 z-30 text-white border-b-2 border-white pr-20">
+                        Kecemasan
+                    </h2>
+                </a>
+            </div>
+            {{-- !: Card Toxic RelationShip --}}
+            <div class="relative m-10">
+                <a href="/posts?category=toxic relationship">
+                    <div wrap-image class="bg-gray-800/70 z-20 absolute rounded-lg"></div>
+                    <img image-gendre src="\asset\img\toxic_relationship.jpg" alt=""
+                        class="rounded-lg z-10 bg-cover w-96">
+                    <h2
+                        class="font-roboto text-3xl font-bold absolute left-8 top-1/2 z-30 text-white border-b-2 border-white pr-20">
+                        Toxic RelationShip
+                    </h2>
+                </a>
+            </div>
+        </div>
+    </div>
 </div>
+<script src="{{ asset('js/artikelBeranda.js') }}"></script>
