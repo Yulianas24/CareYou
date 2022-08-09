11a77b8f11a1708421cd88af8f41313936667824
CoreSinText <muhammadrazak4@gmail.com>
2022-08-10 03:30:59 +0700

    Update header Profile

--- resources/views/components/headerProfile.blade.php
+++ resources/views/components/headerProfile.blade.php
@@ -3,8 +3,8 @@
     @include('/template/templateMain')
 </div>
 {{-- !: Caption --}}
-<div class="w-screen h-52 flex items-center">
-    <figure class="flex flex-col ml-24">
+<div class="w-screen h-52 flex items-center relative">
+    <figure class="flex flex-col absolute left-5">
         <h1 class="font-roboto font-semibold text-2xl">Profile</h1>
         <h3 class="font-poppins text-gray-500 mt-6">Lengkapi profil kamu</h3>
     </figure>
