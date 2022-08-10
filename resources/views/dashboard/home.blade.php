b34c78e40e1b32513c787f49715dbacda261fed7
Yulianas24 <101847329+Yulianas24@users.noreply.github.com>
2022-08-11 00:27:12 +0700

    tambahin chat di dashboard

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -12,7 +12,7 @@
       <tr class="border-b border-gray-500">
         <th class=" p-1 w-12" scope="col">No</th>
         <th class=" p-1 text-left w-44" scope="col">Nama</th>
-        <th class=" p-1 text-center" scope="col">Jenis Kelamin</th>
+        <th class=" p-1 text-center" scope="col">Chat</th>
         <th class=" p-1 text-center" scope="col">Hari</th>
         <th class=" p-1 text-center" scope="col">Jam Konsultasi</th>
         <th class=" p-1 text-center" scope="col">Tanggal</th>
@@ -27,7 +27,11 @@
       <tr class="border-b border-gray-600 ">
         <td class="py-2 text-center">{{ $loop->iteration }}</td>
         <td class="py-2 w-44">{{ $item->konseli->name }}</td>
-        <td class="py-2 text-center">{{ $item->konseli->jenis_kelamin }}</td>
+        <td class="py-2 text-center">
+          <a href="/chatify/{{ $item->konseli->id }}" >
+            <button class="bg-blue-500 hover:bg-blue-700  text-white py-1 px-2 rounded-sm">Kirim Pesan</button>
+          </a>
+        </td>
         <td class="py-2 text-center">{{ $item->hari }}</td>
         
         <td class="py-2 text-center">{{ ($item->jam<10)? '0'.$item->jam : $item->jam }}:00</td>
