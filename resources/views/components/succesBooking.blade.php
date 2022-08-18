1d3c32bad55bbef495bcad5134b7664e042d9718
Yulianas24 <101847329+Yulianas24@users.noreply.github.com>
2022-08-18 11:55:57 +0700

    tambah link success boking

--- resources/views/components/succesBooking.blade.php
+++ resources/views/components/succesBooking.blade.php
@@ -5,9 +5,13 @@
         <h1 class="font-roboto font-medium text-[32px] text-blue-601">Konselor Berhasil di Booking</h1>
     </div>
     <div class="flex px-10 justify-between">
-        <button class="border border-blue-901 text-white rounded-lg font-roboto font-bold py-2 px-8 bg-blue-901"><a
-                href="/dashboard">Home</a></button>
-        <button class="border border-blue-901 text-blue-901 rounded-lg font-roboto font-bold py-2 px-4">Kirim
-            Pesan</button>
+        <a href="/">
+            <button class="border border-blue-901 text-white rounded-lg font-roboto font-bold py-2 px-8 bg-blue-901">Home</button>
+        </a>
+        <a href="/chatify/{{ $konselor->id }} " target="_blank">
+            <button class="border border-blue-901 text-blue-901 rounded-lg font-roboto font-bold py-2 px-4">Kirim
+                Pesan</button>
+        </a>
+        
     </div>
 </div>
