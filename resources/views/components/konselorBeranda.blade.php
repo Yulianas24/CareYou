cbb4b01a16c98e757f9d5847d1e24f77a4b2b8d4
CoreSinText <coresintext@gmail.com>
2022-12-19 14:39:46 +0700

    Memindahkan lihat semua kebawah

--- resources/views/components/konselorBeranda.blade.php
+++ resources/views/components/konselorBeranda.blade.php
@@ -6,16 +6,16 @@
     {{-- ?: Container Text --}}
     <figure class="flex justify-between w-full px-2 laptop:w-1/1.2 laptop:mb-20 desktop:mb-0 desktop:mt-20">
         <h1 class="font-roboto font-medium text-3xl border-b-2 border-blue-601 ">Konselor CareYou</h1>
-        <figcaption class="flex items-center cursor-pointer">
-            <a href="/konselor" class="font-roboto font-semibold text-base text-blue-601">Lihat Semua</a>
-            <img src="/asset/icons/arrow_back.svg" alt="" class="bg-cover">
-        </figcaption>
     </figure>
     {{-- ?: Card Container --}}
     <div class="overflow-auto w-full">
         <figure class="flex min-w-min h-4/5 justify-evenly items-center">
             @include('/partials/cardKonselor')
         </figure>
+        <figcaption class="flex items-center cursor-pointer w-full px-2 laptop:w-1/1.2 justify-end mt-4">
+            <a href="/konselor" class="font-roboto font-semibold text-base text-blue-601">Lihat Semua</a>
+            <img src="/asset/icons/arrow_back.svg" alt="" class="bg-cover">
+        </figcaption>
     </div>
 </div>
 
