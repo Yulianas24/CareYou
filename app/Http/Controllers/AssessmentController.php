30d18925102d9c549b72f9c46d9f236b5b5d294e
Yulianas24 <101847329+Yulianas24@users.noreply.github.com>
2023-03-23 19:48:45 +0700

    add : assess ment

--- app/Http/Controllers/AssessmentController.php
+++ app/Http/Controllers/AssessmentController.php
@@ -7,4 +7,9 @@ use Illuminate\Http\Request;
 class AssessmentController extends Controller
 {
     //
+    public function index() {
+        return view('pages.assessment', [
+
+        ]);
+    }
 }
